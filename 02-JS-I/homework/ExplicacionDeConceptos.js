3609afab686790fdd71f9e54a7681f76df39d17d
Lordkev23 <kevdaviman.23@gmail.com>
2022-09-03 14:25:44 -0500

    Homework JS-I (Explicación Conceptos) terminada, ExplicacionDeConceptos.js

---
+++ 02-JS-I/homework/ExplicacionDeConceptos.js
@@ -0,0 +1,20 @@
+//En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos
+//como si se lo estuvieras explicando a un niño de 12 años.
+//Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+//
+//Variables:
+//Puede almacenar cualquier dato y puede ser usado después.
+//
+//Strings:
+//Es un texto que se coloca entre comillas. De esta manera, se evita que se le confunda,
+// por ejemplo con una función.
+//
+//Funciones (argumentos, return):
+//Son aquellas instrucciones que realizan alguna tarea o actividad.
+//
+//Declaraciones (if):
+//Son instrucciones condicionales. Es decir, realizan una acción determinada si cumplen
+// con una condición especifica.
+//
+//Valores booleanos (true, false):
+//Son respuestas que solo pueden ser de 2 tipos: verdadero (true) o falso (false).
