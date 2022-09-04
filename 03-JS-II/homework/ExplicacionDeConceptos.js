4bddce0dc479265e962339558f786cbc805c0fdd
Lordkev23 <kevdaviman.23@gmail.com>
2022-09-04 17:22:10 -0500

    Homework JS-II terminada (Explicación de Conceptos), ExplicacionDeConceptos.js

---
+++ 03-JS-II/homework/ExplicacionDeConceptos.js
@@ -0,0 +1,22 @@
+//En un archivo de texto separado que debes crear,
+// escribe explicaciones de los siguientes conceptos
+// como si se lo estuvieras explicando a un niño de 12 años. 
+//Hacer esto te ayudará a descubrir rápidamente cualquier
+// agujero en tu comprensión.
+//
+//for :
+//
+//
+//&& :
+//Es un operador lógico que significa 'Y' (And en inglés).
+//Si ambas expresiones son True, te devuelve True. Si almenos
+// una de ellas es False, te devuelve False.
+//
+//|| :
+//También un operador lógico, significa 'O' (Or en inglés).
+//Si ambas expresiones son False, te devuelve False. Si almenos
+// una de ellas es True, te devuelve True.
+//
+//! :
+//Es una negación. Si la Expresión es True, te devuelve False.
+//Su significado es 'NO'(NOT en inglés).
