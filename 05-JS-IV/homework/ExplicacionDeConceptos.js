33d25148ae34b8088be1800851601caf6c5ee6e4
Lordkev23 <kevdaviman.23@gmail.com>
2022-09-06 16:35:08 -0500

    Homework JS-III terminada (Explicación de Conceptos), ExplicacionDeConceptos.js

---
+++ 05-JS-IV/homework/ExplicacionDeConceptos.js
@@ -0,0 +1,24 @@
+//En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos
+// como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a
+// descubrir rápidamente cualquier agujero en tu comprensión.
+//
+//Objetos:
+//Son contenedores de datos que pueden contener mucha información pero de una sola cosa.
+//
+//Propiedades:
+//Es una asociación entre una clave y un valor, siempre y cuando el valor no sea una función.
+//
+//Métodos:
+//Es una asociación entre una clave y un valor que es una función.
+//
+//Bucle for…in:
+//Tipo de bucle usado en Objetos. Un equivalente sería el bucle for en un arreglo(array).
+//
+//Notación de puntos vs notación de corchetes:
+//Notación de puntos: Forma de hacer el llamado a lo que se cuentre en la clave y valor del objeto deseado
+//usando un punto entre la clave y el valor:
+//ej: Key.value;
+//
+//Notación de corchetes: Forma de hacer el llamado a lo que se cuentre en la clave y valor del objeto deseado
+//escribiendo la clave y luego colocando entre corchetes y comillas el valor:
+//ej: key['value'];
