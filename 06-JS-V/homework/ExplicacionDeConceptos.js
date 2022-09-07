8c3f024f70a78c130393ab3106a02ca329ba5557
Lordkev23 <kevdaviman.23@gmail.com>
2022-09-07 11:32:40 -0500

    Homework JS-V terminada (Explicación de Conceptos), ExplicacionDeConceptos.js

---
+++ 06-JS-V/homework/ExplicacionDeConceptos.js
@@ -0,0 +1,11 @@
+//En un archivo de texto separado que debes crear, escribe explicaciones de los
+// siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. 
+//Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+//prototype:
+//Es una forma de establecer un método una vez y que y que cada objeto de esa clase
+// tenga acceso a eso método .
+//
+//Constructors (de Clases):
+//Permite crear muchos objetos con las mismas propiedades y métodos.
+//ej: los objetos serían los usuario de facebook, todos comparten ciertas propiedades
+// y métodos como edad, sexo, gustos(), etc.
