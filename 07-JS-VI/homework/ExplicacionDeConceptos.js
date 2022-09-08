813a88b08bc2a2d8ddaf16746e6b3f576330f2f6
Lordkev23 <kevdaviman.23@gmail.com>
2022-09-08 18:49:40 -0500

    Homework JS-VI terminada (Explicación de Conceptos), ExplicacionDeConceptos.js

---
+++ 07-JS-VI/homework/ExplicacionDeConceptos.js
@@ -0,0 +1,8 @@
+//En un archivo de texto separado que debes crear, escribe explicaciones de
+// los siguientes conceptos como si se lo estuvieras explicando a un niño
+// de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier
+// agujero en tu comprensión.
+//
+//Funciones Callback:
+//Es la capacidad de pasar una función como argumento a otra función. Se usa
+//el argumento cb como nexo para la función que se usará de callback.
